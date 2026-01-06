8dfd943dc7610181a14abfbd09d70339f6250d22
GoutamVaishnav <goutamvaishnav09@gmail.com>
2026-01-06 14:58:37 +0530

    normal change

--- frontend/src/components/Loadingg.tsx
+++ frontend/src/components/Loadingg.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Loadingg = () => {
   return (
-    <div className="w-[200px] m-auto mt-[200px]">
+    <div className="w-50 m-auto mt-50">
       <p className="text-2xl text-blue-500 font-bold text-center">loading...</p>
     </div>
   );
